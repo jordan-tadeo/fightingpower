94fd05330f63fc7b676de047ab75ed41ed006cec
Jordan Tadeo <jtadeo128@gmail.com>
2022-05-28 04:48:54 -0700

    >:(

--- main.py
+++ main.py
@@ -3,24 +3,42 @@ import pygame
 
 W, H = 720, 480
 
-screen = pygame.display.set_mode((W, H))
+RED_BROWN = (200, 90, 85)
+BLUE = (28, 98, 128)
 
-# Variable to keep the main loop running
-running = True
 
-# Main loop
-while running:
-    # Look at every event in the queue
-    for event in pygame.event.get():
-        # Did the user hit a key?
-        if event.type == pygame.KEYDOWN:
-            # Was it the Escape key? If so, stop the loop.
-            if event.key == pygame.K_ESCAPE:
+if __name__ == '__main__':
+    pygame.init()
+    
+    screen = pygame.display.set_mode((W, H))
+
+    floor = pygame.Surface((W, 32))
+    floor.fill(RED_BROWN)
+    floor_coord = (0, H - 32)
+
+    player = pygame.Surface((32, 32))
+    player.fill(BLUE)
+    player_coord = [16, H - 64]
+
+    # Variable to keep the main loop running
+    running = True
+    # Main loop
+    while running:
+        screen.fill((0,0,0))
+        screen.blit(floor, floor_coord)
+        screen.blit(player, player_coord)
+        pygame.display.flip()
+
+        # Look at every event in the queue
+        for event in pygame.event.get():
+            # Did the user hit a key?
+            if event.type == pygame.KEYDOWN:
+                # Was it the Escape key? If so, stop the loop.
+                if event.key == pygame.K_ESCAPE:
+                    running = False
+                if event.key == pygame.K_d:
+                    player_coord[0] += 16
+
+            # Did the user click the window close button? If so, stop the loop.
+            elif event.type == pygame.QUIT:
                 running = False
-
-        # Did the user click the window close button? If so, stop the loop.
-        elif event.type == pygame.QUIT:
-            running = False
-
-
-pygame.init()
